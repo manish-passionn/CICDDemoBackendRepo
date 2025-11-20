1230f9268c7888e68c7b245065d00602d1c44ad2
manish-passionn <manish.passionn@gmail.com>
2025-11-20 18:41:49 +0530

    Add files via upload

---
+++ src/main/java/com/bank/allservices/config/DynamicCorsFilter.java
@@ -0,0 +1,28 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+@Component
+public class DynamicCorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        String origin = request.getHeader("Origin");
+        if (origin != null) {
+            response.setHeader("Access-Control-Allow-Origin", origin);
+            response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+        }
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, res);
+        }
+    }
+}
