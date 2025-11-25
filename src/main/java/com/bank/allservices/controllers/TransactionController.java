0e6545bf7e7a53fd36a1fd6aeded4dab8e1c41bc
manish-passionn <manish.passionn@gmail.com>
2025-11-25 16:30:29 +0530

    Update TransactionController.java

--- src/main/java/com/bank/allservices/controllers/TransactionController.java
+++ src/main/java/com/bank/allservices/controllers/TransactionController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.bank.allservices.models.TransactionEntity;
 
 import com.bank.allservices.services.JWTService;
