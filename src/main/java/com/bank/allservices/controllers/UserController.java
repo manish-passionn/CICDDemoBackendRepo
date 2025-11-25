a229a2271c459162fd1e919925e88425dff19948
manish-passionn <manish.passionn@gmail.com>
2025-11-25 16:30:45 +0530

    Update UserController.java

--- src/main/java/com/bank/allservices/controllers/UserController.java
+++ src/main/java/com/bank/allservices/controllers/UserController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import com.bank.allservices.models.JwtResponse;
 import com.bank.allservices.models.LoginEntity;
 import com.bank.allservices.models.TransactionEntity;
